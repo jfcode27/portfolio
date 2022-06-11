0f9820585c85c5e2d52d2e2e7d65f13f41014eeb
Jeremy Franco <50761613+jfcode27@users.noreply.github.com>
2022-06-11 16:02:20 -0500

    Seo

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Helmet from "react-helmet";
 import { Contact, Featured, Intro, WhoIm } from "../components/sections";
 
 const Index = () => {
   return (
     <>
+    <Helmet>
+        <meta name="description" content="Jeremy Franco's personal website."/>
+    </Helmet>
       <Intro />
       <WhoIm />
       <Featured />
